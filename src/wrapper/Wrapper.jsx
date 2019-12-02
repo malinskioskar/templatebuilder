3fa9421fa4037fa9b9268373c7ceb8e0f1ffde10
Oskar Malinski <Oskar.Malinski@Gamesys.co.uk>
2019-12-02 15:42:35 +0000

    added wrapper and board

---
+++ src/wrapper/Wrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from '../board/Board';
+
+const Wrapper = () => {
+    return (
+        <div>
+            <Board>
+                SOMETHING
+            </Board>
+        </div>
+    )
+}
+export default Wrapper;
